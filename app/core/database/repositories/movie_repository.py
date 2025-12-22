1a11d3c9b611d9de00c4a1cc66f335e4f6aeefb2
Sergio Balerdi <pegasus.prodescargas@gmail.com>
2025-12-22 20:51:02 -0300

    sacar logger de modulos internos

--- app/core/database/repositories/movie_repository.py
+++ app/core/database/repositories/movie_repository.py
@@ -1,14 +1,7 @@
+from sqlalchemy.orm import Session
 from .base_repository import BaseRepository
 from ..models.models import Movie
-from sqlalchemy.orm import Session
 
-
-import logging
-import app.config.config as config
-
-
-logging.basicConfig(level=config.LOG_LEVEL)
-logger = logging.getLogger(__name__)
 
 class MovieRepository(BaseRepository[Movie]):
     def __init__(self, session: Session):
