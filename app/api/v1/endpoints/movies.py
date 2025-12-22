13144fffefd1f773f06de056562403b8ff0deef2
Sergio Balerdi <pegasus.prodescargas@gmail.com>
2025-12-22 19:52:32 -0300

    endpoints para movie

--- app/api/v1/endpoints/movies.py
+++ app/api/v1/endpoints/movies.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from app.api.v1.schemas.movies import MovieCreate
 from app.config.config import config
 import logging
 
@@ -13,3 +14,11 @@ router = APIRouter()
 def read_hello():
     """Endpoint principal de la API."""
     return {"message": "Bienvenido al Catálogo de Películas 🎬"}
+
+@router.post('/movie')
+async def create_movie(request: MovieCreate):
+    return {
+        "success": True,
+        "message": "Película creada exitosamente",
+        "data": request.model_dump()
+    }
