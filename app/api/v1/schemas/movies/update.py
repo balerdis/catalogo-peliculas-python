e346100255fb3834501b450373c40dd3318a1da1
Sergio Balerdi <pegasus.prodescargas@gmail.com>
2026-01-07 18:41:34 -0300

    chequeo los datos de entrada con un mejor schema de validacion

--- app/api/v1/schemas/movies/update.py
+++ app/api/v1/schemas/movies/update.py
@@ -1,12 +1,4 @@
-from pydantic import BaseModel
+from app.api.v1.schemas.movies.base import MovieBase
 
-class MovieUpdate(BaseModel):
-    title: str | None = None
-    director: str | None = None
-    year: int | None = None
-    genre_id: int | None = None
-    price: float | None = None
-    duration: int | None = None
-    rating: int | None = None
-    description: str | None = None
-    is_watched: bool | None = None
+class MovieUpdate(MovieBase):
+    pass
